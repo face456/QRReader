d9273ec2597e7dda9bbae07747563b0b8ccff07b
Philipp Jahn <philipp.jahn@rollindonkey.com>
2017-09-23 12:29:48 +0200

    Project completed

---
+++ QRReader/Controller/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  QRReader
+//
+//  Created by Philipp Jahn on 23.09.17.
+//  Copyright © 2017 Philipp Jahn. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    @IBOutlet weak var square: UIImageView!
+    var video = AVCaptureVideoPreviewLayer()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        captureSession()
+        
+    }
+    
+    func captureSession() {
+        // Creating Session
+        let session = AVCaptureSession()
+        
+        //Define capture device
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do
+        {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            session.addInput(input)
+        }
+        catch
+        {
+            print ("Error")
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        
+        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        
+        self.view.bringSubview(toFront: square)
+        
+        session.startRunning()
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects.count != 0 {
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject {
+                if object.type == AVMetadataObject.ObjectType.qr {
+                    let alert = UIAlertController(title: "QR Code", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: { (nil) in
+                        UIPasteboard.general.string = object.stringValue
+                    }))
+                    present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
